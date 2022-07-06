a503f6f1ba53db2a9638e392d4300c83b87ba96c
Haytam <haytamkh7@gmail.com>
2022-07-06 10:32:46 +0300

    Mobile Testing

--- test_amazon_chrome.py
+++ test_amazon_chrome.py
@@ -1,33 +1,34 @@
-# import time
-# from selenium.webdriver.chrome import webdriver
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# import pytest
-#
-#
-# @pytest.fixture()
-# def driver():
-#     # chrome_driver_binary = "chromedriver.exe"
-#     # ser_chrome = ChromeService(chrome_driver_binary)
-#
-#     # driver = webdriver.Chrome(service=ser_chrome)
-#     # Headless
-#     dc = {
-#         "browserName": "chrome",
-#         "platformName": "Windows 11"
-#
-#     }
-#     # Selenium grid standAlone
-#     driver = webdriver.Remote("http://localhost:4444", desired_capabilities=dc)
-#     yield driver
-#     driver.close()
-#     # java -jar selenium-server-4.2.2.jar standalone
-#
-#
-# def test_open_webpage(driver):
-#     # Open the web page
-#     driver.get('https://www.amazon.com/')
-#     time.sleep(3)
+import time
+from selenium.webdriver.chrome import webdriver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+
+
+@pytest.fixture()
+def driver():
+    # chrome_driver_binary = "chromedriver.exe"
+    # ser_chrome = ChromeService(chrome_driver_binary)
+
+    # driver = webdriver.Chrome(service=ser_chrome)
+    # Headless
+    dc = {
+        "browserName": "chrome",
+        "platformName": "Windows 11"
+
+    }
+    # Selenium grid standAlone
+    driver = webdriver.Remote("http://localhost:4444", desired_capabilities=dc)
+    yield driver
+    driver.close()
+    # java -jar selenium-server-4.3.0.jar standalone
+
+
+def test_open_webpage(driver):
+    # Open the web page
+    driver.get('https://www.amazon.com/')
+    time.sleep(3)
+    assert driver.title == "Google"
 #
 #
 # def test_register(driver):
